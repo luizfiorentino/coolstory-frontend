cdb425687b971974d6296d458cd2a9621be4c226
luizfiorentino <lgfiorentino@gmail.com>
2022-11-22 16:54:53 +0100

    style- Space Details Page showing header and stories using flexbox model

--- src/components/StoryCard/index.js
+++ src/components/StoryCard/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import "./styles.css";
 
 export default function StoryCard(props) {
   return (
     <div>
-      <h3>{props.name}</h3>
+      {/* <h3>{props.name}</h3>
       <h4>{props.content}</h4>
-      <img src={props.imageUrl} alt="foto from story" style={{ width: 500 }} />
+      <img src={props.imageUrl} alt="foto from story" style={{ width: 500 }} /> */}
+      <div className="story-card-main">
+        <div className="story-title">
+          <h3>{props.name}</h3>
+        </div>
+
+        <div className="story-image">
+          <img src={props.imageUrl} alt="foto from story" />
+        </div>
+        <div className="story-content">
+          <p>{props.content}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/SpaceDetails/index.js
+++ src/pages/SpaceDetails/index.js
@@ -6,6 +6,7 @@ import { spaceDetailsThunk } from "../../store/spaces/thunks";
 import { selectSpaceDetails } from "../../store/spaces/selectors";
 import { useParams } from "react-router-dom";
 import StoryCard from "../../components/StoryCard";
+import "./styles.css";
 
 export default function SpaceDetails() {
   const dispatch = useDispatch();
@@ -21,18 +22,39 @@ export default function SpaceDetails() {
   const backgroundColor = details?.backgroundColor;
 
   return (
-    <div style={{ color: colorDetails, background: backgroundColor }}>
-      <h2>Space Details</h2>
-      <h3>{details?.title}</h3>
-      <h4>{details?.description}</h4>
-      {details?.stories?.map((story) => (
-        <StoryCard
-          id={story.id}
-          name={story.name}
-          content={story.content}
-          imageUrl={story.imageUrl}
-        />
-      ))}
+    <div className="my-space-main-container">
+      <h4>Space Details</h4>
+      <div
+        className="main-container"
+        style={{ color: colorDetails, background: backgroundColor }}
+      >
+        <div className="space-header">
+          <div className="user-space-title">
+            <h2 className="title">{details?.title}</h2>
+          </div>
+          <div className="user-space-description">
+            <h4>{details?.description}</h4>
+          </div>
+          <div className="user-stories-call">
+            {details?.stories?.length === 0 || !details?.stories ? (
+              <h5>User posted no stories yet</h5>
+            ) : (
+              <h5>Check out my stories</h5>
+            )}
+          </div>
+        </div>
+
+        <div className="stories-container">
+          {details?.stories?.map((story) => (
+            <StoryCard
+              id={story.id}
+              name={story.name}
+              content={story.content}
+              imageUrl={story.imageUrl}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
